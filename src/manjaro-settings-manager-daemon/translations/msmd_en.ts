b3694c410176bc891faf3665a065228ee01d3797
Philip <philm@manjaro.org>
2014-07-17 08:43:43 +0200

    Translations - update ts-files

--- src/manjaro-settings-manager-daemon/translations/msmd_en.ts
+++ src/manjaro-settings-manager-daemon/translations/msmd_en.ts
@@ -4,7 +4,7 @@
 <context>
     <name>Daemon</name>
     <message numerus="yes">
-        <location filename="../daemon.cpp" line="66"/>
+        <location filename="../daemon.cpp" line="88"/>
         <source>Additional Language Package(s)</source>
         <translation type="unfinished">
             <numerusform></numerusform>
@@ -12,12 +12,30 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="../daemon.cpp" line="67"/>
+        <location filename="../daemon.cpp" line="89"/>
         <source>%n new additional language package(s) available</source>
         <translation type="unfinished">
             <numerusform></numerusform>
             <numerusform></numerusform>
         </translation>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="125"/>
+        <location filename="../daemon.cpp" line="135"/>
+        <location filename="../daemon.cpp" line="143"/>
+        <source>Running an unsupported kernel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="126"/>
+        <location filename="../daemon.cpp" line="136"/>
+        <source>You are running a kernel which is unsupported, please update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="144"/>
+        <source>You have installed a kernel which is unsupported.</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
