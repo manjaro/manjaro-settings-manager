cd2bfe946b950d0afc5ff6b968da3702d8bc3604
Philip <philm@manjaro.org>
2014-07-24 00:05:20 +0200

    Update translations: el, zh_TW

--- src/manjaro-settings-manager-daemon/translations/msmd_el.ts
+++ src/manjaro-settings-manager-daemon/translations/msmd_el.ts
@@ -2,42 +2,42 @@
 <context>
     <name>Daemon</name>
     <message numerus="yes">
-        <location filename="../daemon.cpp" line="88"/>
+        <location filename="../daemon.cpp" line="90"/>
         <source>Additional Language Package(s)</source>
         <translation type="unfinished"><numerusform></numerusform><numerusform></numerusform></translation>
     </message>
     <message>
-        <location filename="../daemon.cpp" line="205"/>
+        <location filename="../daemon.cpp" line="186"/>
         <source>Running an unsupported kernel, please update</source>
         <translation>Τρέχει ένα πυρήνα που δεν υποστηρίζονται, παρακαλώ αναβαθμίσετε</translation>
     </message>
     <message>
-        <location filename="../daemon.cpp" line="207"/>
+        <location filename="../daemon.cpp" line="188"/>
         <source>Unsupported kernel installed in your system.</source>
         <translation>Ανυποστηρικτη πυρήνα εγκατεστημένο στο σύστημά σας.</translation>
     </message>
     <message>
-        <location filename="../daemon.cpp" line="211"/>
+        <location filename="../daemon.cpp" line="191"/>
         <source>Your kernels need attention.</source>
         <translation>Σας πυρήνες χρειάζονται προσοχή</translation>
     </message>
     <message>
-        <location filename="../daemon.cpp" line="214"/>
+        <location filename="../daemon.cpp" line="194"/>
         <source>Unsupported Kernel Found.</source>
         <translation>Ανυποστηρικτη Πυρήνας βρέθηκε.</translation>
     </message>
     <message>
-        <location filename="../daemon.cpp" line="216"/>
+        <location filename="../daemon.cpp" line="196"/>
         <source>New Kernel Available.</source>
-        <translation>Νέας Πυρήνες Διαθέσιμες</translation>
+        <translation>Νέα Πυρήνα Διαθέσιμο</translation>
     </message>
     <message>
-        <location filename="../daemon.cpp" line="220"/>
+        <location filename="../daemon.cpp" line="199"/>
         <source>A kernel newer than the latest installed is available.</source>
         <translation>Ένα πυρήνα νεώτερο από τα τελευταία εγκατεστημένο είναι διαθέσιμα</translation>
     </message>
     <message numerus="yes">
-        <location filename="../daemon.cpp" line="86"/>
+        <location filename="../daemon.cpp" line="88"/>
         <source>%n new additional language package(s) available</source>
         <translation type="unfinished"><numerusform></numerusform><numerusform></numerusform></translation>
     </message>
--- src/manjaro-settings-manager-daemon/translations/msmd_zh_TW.ts
+++ src/manjaro-settings-manager-daemon/translations/msmd_zh_TW.ts
@@ -2,42 +2,42 @@
 <context>
     <name>Daemon</name>
     <message numerus="yes">
-        <location filename="../daemon.cpp" line="88"/>
+        <location filename="../daemon.cpp" line="90"/>
         <source>Additional Language Package(s)</source>
         <translation><numerusform>額外的語言包</numerusform></translation>
     </message>
     <message>
-        <location filename="../daemon.cpp" line="205"/>
+        <location filename="../daemon.cpp" line="186"/>
         <source>Running an unsupported kernel, please update</source>
         <translation>正在執行不支援的核心，請升級</translation>
     </message>
     <message>
-        <location filename="../daemon.cpp" line="207"/>
+        <location filename="../daemon.cpp" line="188"/>
         <source>Unsupported kernel installed in your system.</source>
         <translation>您的系統中安裝著不被支援的核心。</translation>
     </message>
     <message>
-        <location filename="../daemon.cpp" line="211"/>
+        <location filename="../daemon.cpp" line="191"/>
         <source>Your kernels need attention.</source>
-        <translation type="unfinished"/>
+        <translation>您的核心需要被關心一下。</translation>
     </message>
     <message>
-        <location filename="../daemon.cpp" line="214"/>
+        <location filename="../daemon.cpp" line="194"/>
         <source>Unsupported Kernel Found.</source>
         <translation>找到不支援的核心。</translation>
     </message>
     <message>
-        <location filename="../daemon.cpp" line="216"/>
+        <location filename="../daemon.cpp" line="196"/>
         <source>New Kernel Available.</source>
         <translation>有新的核心可供使用。</translation>
     </message>
     <message>
-        <location filename="../daemon.cpp" line="220"/>
+        <location filename="../daemon.cpp" line="199"/>
         <source>A kernel newer than the latest installed is available.</source>
         <translation>比已安裝的核心更新的核心已經可以使用。</translation>
     </message>
     <message numerus="yes">
-        <location filename="../daemon.cpp" line="86"/>
+        <location filename="../daemon.cpp" line="88"/>
         <source>%n new additional language package(s) available</source>
         <translation><numerusform>%n 個新的額外語言包已可使用</numerusform></translation>
     </message>
