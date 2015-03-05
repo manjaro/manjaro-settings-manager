fdd395bcc90c50cd145dcaf99eb092b4bae9a049
Ramon Buldó <rbuldo@gmail.com>
2015-03-05 20:48:02 +0100

    Update translations: ar, is, lt, ml, sr_RS, sr_RS@latin, uz@Latn

---
+++ src/manjaro-settings-manager-daemon/translations/msmd_lt.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="lt" version="2.1">
+<context>
+    <name>Daemon</name>
+    <message numerus="yes">
+        <location filename="../daemon.cpp" line="92"/>
+        <source>Additional Language Package(s)</source>
+        <translation><numerusform>Papildomas Kalbos Paketas(-ai)</numerusform><numerusform>Papildomi Kalbos Paketai(-as)</numerusform><numerusform>Papildomų Kalbos Paketų(-ai)</numerusform></translation>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="176"/>
+        <source>Running an unsupported kernel, please update</source>
+        <translation>Vykdomas nepalaikomas branduolys, prašome atsinaujinti</translation>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="178"/>
+        <source>Unsupported kernel installed in your system.</source>
+        <translation>Jūsų sistemoje yra įdiegtas nepalaikomas branduolys.</translation>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="181"/>
+        <source>Your kernels need attention.</source>
+        <translation>Jūsų branduoliai reikalauja dėmesio.</translation>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="184"/>
+        <source>Unsupported Kernel Found.</source>
+        <translation>Rastas Nepalaikomas Branduolys.</translation>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="186"/>
+        <source>New Kernel Available.</source>
+        <translation>Prieinamas Naujas Branduolys.</translation>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="189"/>
+        <source>A kernel newer than the latest installed is available.</source>
+        <translation>Yra prieinamas naujesnis, nei paskiausiai įdiegtas, branduolys.</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../daemon.cpp" line="90"/>
+        <source>%n new additional language package(s) available</source>
+        <translation><numerusform>Yra prieinamas %n naujas papildomas kalbos paketas(-ai)</numerusform><numerusform>Yra prieinami %n nauji papildomi kalbos paketai(-as)</numerusform><numerusform>Yra prieinama %n naujų papildomų kalbos paketų(-ai)</numerusform></translation>
+    </message>
+</context>
+</TS>
---
+++ src/manjaro-settings-manager-daemon/translations/msmd_sr_RS.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sr_RS" version="2.1">
+<context>
+    <name>Daemon</name>
+    <message numerus="yes">
+        <location filename="../daemon.cpp" line="92"/>
+        <source>Additional Language Package(s)</source>
+        <translation type="unfinished"><numerusform></numerusform><numerusform></numerusform><numerusform></numerusform></translation>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="176"/>
+        <source>Running an unsupported kernel, please update</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="178"/>
+        <source>Unsupported kernel installed in your system.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="181"/>
+        <source>Your kernels need attention.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="184"/>
+        <source>Unsupported Kernel Found.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="186"/>
+        <source>New Kernel Available.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../daemon.cpp" line="189"/>
+        <source>A kernel newer than the latest installed is available.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message numerus="yes">
+        <location filename="../daemon.cpp" line="90"/>
+        <source>%n new additional language package(s) available</source>
+        <translation type="unfinished"><numerusform></numerusform><numerusform></numerusform><numerusform></numerusform></translation>
+    </message>
+</context>
+</TS>
