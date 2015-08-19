5a14781e38cffc0ebb454d8ad2234a526a53d3d0
Ramon Buldó <rbuldo@gmail.com>
2015-08-19 02:33:06 +0200

    mhwd helper: make remove function call install, add comments, add copyright
    Process will wait for 10 minutes maximum to finish.

--- src/modules/mhwd/MhwdAuthHelper.cpp
+++ src/modules/mhwd/MhwdAuthHelper.cpp
@@ -1,3 +1,23 @@
+/*
+ *  Manjaro Settings Manager
+ *  Ramon Buldo <ramon@manjaro.org>
+ *
+ *  Copyright (C) 2007 Free Software Foundation, Inc.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "MhwdAuthHelper.h"
 
 #include <QtCore/QProcess>
@@ -5,41 +25,31 @@
 
 ActionReply MhwdAuthHelper::install(const QVariantMap& args)
 {
-    QProcess *mhwd = new QProcess();
-    mhwd->start("/usr/bin/mhwd", args["arguments"].toStringList());
-    connect(mhwd, &QProcess::readyRead,
-            [=] ()
-    {
-        QString data = QString::fromUtf8(mhwd->readAll()).trimmed();
-        if (!data.isEmpty()) {
-            QVariantMap map;
-            map.insert(QLatin1String("Data"), data);
-            HelperSupport::progressStep(map);
-        }
-    });
-    mhwd->waitForStarted();
-    mhwd->waitForFinished(-1);
+    QProcess mhwd;
+    mhwd.start("/usr/bin/mhwd", args["arguments"].toStringList());
+    connect(&mhwd, &QProcess::readyRead,
+            [&] ()
+            {
+                QString data = QString::fromUtf8(mhwd.readAll()).trimmed();
+                if (!data.isEmpty()) {
+                    QVariantMap map;
+                    map.insert(QLatin1String("Data"), data);
+                    HelperSupport::progressStep(map);
+                }
+            });
+    if (!mhwd.waitForStarted()) {
+        return ActionReply::HelperErrorReply();
+    }
+    if (!mhwd.waitForFinished(600000)) {
+        return ActionReply::HelperErrorReply();
+    }
     return ActionReply::SuccessReply();
 }
 
 
 ActionReply MhwdAuthHelper::remove(const QVariantMap& args)
 {
-    QProcess *mhwd = new QProcess();
-    mhwd->start("/usr/bin/mhwd", args["arguments"].toStringList());
-    connect(mhwd, &QProcess::readyRead,
-            [=] ()
-    {
-        QString data = QString::fromUtf8(mhwd->readAll()).trimmed();
-        if (!data.isEmpty()) {
-            QVariantMap map;
-            map.insert(QLatin1String("Data"), data);
-            HelperSupport::progressStep(map);
-        }
-    });
-    mhwd->waitForStarted();
-    mhwd->waitForFinished(-1);
-    return ActionReply::SuccessReply();
+    return install(args);
 }
 
 KAUTH_HELPER_MAIN("org.manjaro.msm.mhwd", MhwdAuthHelper)
