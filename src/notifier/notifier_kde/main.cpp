85dcc6dabed83c114d742ae227f3801edf60b5e8
scachemaille <stephane@scachemaille.ch>
2016-04-29 22:56:46 +0200

    Add a second notifier using Knotify
    Modify folder structure due to the new notifier

---
+++ src/notifier/notifier_kde/main.cpp
@@ -0,0 +1,56 @@
+/*
+ *  This file is part of Manjaro Settings Manager.
+ *
+ *  Ramon Buldó <ramon@manjaro.org>
+ *
+ *  Manjaro Settings Manager is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Manjaro Settings Manager is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Manjaro Settings Manager.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "NotifierApp.h"
+#include "Notifier.h"
+#include "kdsingleapplicationguard/kdsingleapplicationguard.h"
+
+#include <QtWidgets/QApplication>
+#include <QtCore/QTranslator>
+#include <QtCore/QFile>
+#include <QtCore/QDir>
+
+#include <QDebug>
+
+int main( int argc, char* argv[] )
+{
+    NotifierApp app( argc, argv );
+
+    Q_INIT_RESOURCE( language_packages );
+    Q_INIT_RESOURCE( translations );
+
+    QTranslator appTranslator;
+    appTranslator.load( ":/translations/msmd_" + QLocale::system().name() );
+    app.installTranslator( &appTranslator );
+
+    KDSingleApplicationGuard guard( KDSingleApplicationGuard::AutoKillOtherInstances );
+
+    int returnCode = 0;
+    if ( guard.isPrimaryInstance() )
+    {
+        app.init();
+        Notifier notifier( &app );
+        returnCode = app.exec();
+    }
+    else
+        qDebug() << "MSM Notifier is already running, shutting down.";
+
+    return returnCode;
+}
+
