80a75d1a5dba7a92572ac2ca0e5a6670f905b2a8
Ramon Buldó <rbuldo@gmail.com>
2017-03-04 17:24:23 +0100

    Version 0.5.1

--- src/msm/MsmApplication.cpp
+++ src/msm/MsmApplication.cpp
@@ -31,7 +31,7 @@ MsmApplication::MsmApplication( int& argc, char* argv[] )
     setOrganizationName( "Manjaro" );
     setOrganizationDomain( "Manjaro" );
     setApplicationName( "Manjaro Settings Manager" );
-    setApplicationVersion( "0.5.0" );
+    setApplicationVersion( "0.5.1" );
 }
 
 
