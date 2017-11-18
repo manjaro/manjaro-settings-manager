503bad312fa32fcadf2b0e72a341844b7c5b7579
Philip <philm@manjaro.org>
2017-11-18 09:57:29 -0500

    [libmsm] recommend v4.14

--- src/libmsm/KernelModel.cpp
+++ src/libmsm/KernelModel.cpp
@@ -360,14 +360,14 @@ KernelModel::getRunningKernel() const
 QStringList
 KernelModel::getLtsKernels() const
 {
-    return QStringList() << "linux310" << "linux312" << "linux314" << "linux316" << "linux318" << "linux41" << "linux44" << "linux49";
+    return QStringList() << "linux310" << "linux312" << "linux314" << "linux316" << "linux318" << "linux41" << "linux44" << "linux49" << "linux414";
 }
 
 
 QStringList
 KernelModel::getRecommendedKernels() const
 {
-    return QStringList() << "linux49";
+    return QStringList() << "linux414";
 }
 
 
