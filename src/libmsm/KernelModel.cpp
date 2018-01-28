0ed018a492131721b331a80babb51426184eb443
Philip <philm@manjaro.org>
2018-01-28 05:43:09 -0500

    [libmsm] mark linux414-rt as LTS

--- src/libmsm/KernelModel.cpp
+++ src/libmsm/KernelModel.cpp
@@ -360,7 +360,7 @@ KernelModel::getRunningKernel() const
 QStringList
 KernelModel::getLtsKernels() const
 {
-    return QStringList() << "linux310" << "linux312" << "linux314" << "linux316" << "linux318" << "linux41" << "linux44" << "linux49" << "linux414";
+    return QStringList() << "linux310" << "linux312" << "linux314" << "linux316" << "linux318" << "linux41" << "linux44" << "linux49" << "linux414" << "linux414-rt";
 }
 
 
